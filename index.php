66f39f594b7f3e815dac460a3d35b995cfbde63a
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-12 14:19:01 +0100

    Add: first print on screen, WIP: condition to parking true/false

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+foreach ($hotels as $hotel) {
+    echo '<h2>' . $hotel['name'] . '</h2>';
+    echo '<p>' . $hotel ['description'] . '</p>';
+    echo '<p>' . $hotel['description'] . '</p>';
+    echo '<p> Parking:' . ($hotel['parking']) ? 'yes' : 'no' . '</p>';
+    echo '<p> vote:' . $hotel['vote']  . '</p>';
+    echo '<p> Distance to center:' . $hotel['distance_to_center']  . 'km </p>';
+    echo '<hr>';
+
+
+}
+?>
+    
+</body>
+</html>
