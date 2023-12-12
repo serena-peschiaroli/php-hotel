b1e7b0f654f707198d22bad062655200589bce3f
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-12 14:31:18 +0100

    Add: readme; bootstrap cnd

--- index.php
+++ index.php
@@ -46,32 +46,34 @@ $hotels = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotel details</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
 
-<?php 
-//iterare attraverso hotel in array
-foreach ($hotels as $hotel) {
-    //nome hotel  
-    echo '<h2>' . $hotel['name'] . '</h2>';
-    //descrizione htl
-    echo '<p>' . $hotel['description'] . '</p>';
-    //parcheggio htl con operatore ternario, stampa si se true o no se false
-    echo '<p> Parking:' . ($hotel['parking'] ? 'yes' : 'no') . '</p>';
-    //stampa voto hotel
-    echo '<p> vote:' . $hotel['vote']  . '</p>';
-    //stampa distanza dal centro
-    echo '<p> Distance to center:' . $hotel['distance_to_center']  . 'km </p>';
-    //linea di stacco
-    echo '<hr>';
+    <?php
+    //iterare attraverso hotel in array
+    foreach ($hotels as $hotel) {
+        //nome hotel  
+        echo '<h2>' . $hotel['name'] . '</h2>';
+        //descrizione htl
+        echo '<p>' . $hotel['description'] . '</p>';
+        //parcheggio htl con operatore ternario, stampa si se true o no se false
+        echo '<p> Parking:' . ($hotel['parking'] ? 'yes' : 'no') . '</p>';
+        //stampa voto hotel
+        echo '<p> vote:' . $hotel['vote']  . '</p>';
+        //stampa distanza dal centro
+        echo '<p> Distance to center:' . $hotel['distance_to_center']  . 'km </p>';
+        //linea di stacco
+        echo '<hr>';
+    }
+    ?>
 
-
-}
-?>
-    
 </body>
+
 </html>
