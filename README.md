212a7205c3e59425543a03cb8e8db8e084369f92
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-12 17:09:06 +0100

    Add: filter for vote. Fix: returning 'all' when no filter is selected. WIP: better condition for stars filter

--- README.md
+++ README.md
@@ -13,12 +13,16 @@ Iniziate in modo graduale.
 - [X] Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
 
 Bonus:
-- [X] 1) Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+- [X] 1- Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
 **soluzione:**
   - impostare un form con metodo GET;
-  - prima del forEach, assicurarsi che il filtro del GET sia stato impostato; SE è stato impostato APPLICARLO, ALTRIMENTI ritornare tutti gli htl senza variazioni;
+  - prima del forEach, assicurarsi che il filtro del GET sia stato impostato; SE è stato impostato APPLICARLO, ALTRIMENTI ritornare tutti gli htl senza variazioni ---> aggiunta: impostare <option selected value=""></option> ed aggiungere nella condizione SE il filtro è preso dal GET ; && il filtro è diverso da stringa vuota (true o false)---> Se le condizioni sono true, ritorna il filtro; se false, ritorna il valore 'all';
   - all'interno del foreach, applicare il filtro: SE filtroxparcheggio == 'tutti', tutti gli htl; SE filtroxparcheggio == 'con parcheggio' &&parcheggio == true; SE filtroxparcheggio == 'senza parcheggio' && parcheggio == false; eseguire il codice.
-  
-2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+
+- [X] 2- Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
 Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+
+**soluzione:**
+ - aggiungere un secondo select (resta metodo get); come nel primo caso, controllare se il filtro è stato impostato, altrimenti return 'all'; 
+ - all'interno del foreach, applicare il filtro con tutte le condizioni; eseguire il codice
--- index.php
+++ index.php
@@ -55,63 +55,103 @@ $hotels = [
 </head>
 
 <body>
-    <form method="GET">
-        <select class="form-select" aria-label="Default select example" name="parkingFilter" id="parkingFilter">
-            <option selected>Seleziona: </option>
-            <option value="all">Tutti</option>
-            <option value="yes">Con parcheggio</option>
-            <option value="no">Senza Parcheggio</option>
-        </select>
-        <button type="submit" class="btn btn-primary">INVIA</button>
-    </form>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <form method="GET" class="form-control-sm">
+                <div class="row p-5">
+                    <div class="col">
+                        <select class="form-select form-select-sm" aria-label="Default select example" name="parkingFilter" id="parkingFilter">
+                            <!-- inserire "" in option selected x mostrare comunque tutti gli htls se non si effettua scelta -->
+                            <option selected value="">Filtra per parcheggio: </option>
+                            <option value="all">Tutti</option>
+                            <option value="yes">Con parcheggio</option>
+                            <option value="no">Senza Parcheggio</option>
+                        </select>
+                    </div>
+
+                    <div class="col">
+                        <select class="form-select form-select-sm" aria-label="Default select example" name="starsFilter" id="starsFilter">
+                            <option selected value="">Filtra per voto: </option>
+                            <option value="all">Tutti</option>
+                            <option value="1">Solo 1 stella</option>
+                            <option value="2">Da 2 stelle in su</option>
+                            <option value="3">Da 3 stelle in su</option>
+                            <option value="4">Da 4 stelle in su</option>
+                            <option value="5">Solo 5 stelle</option>
+                        </select>
+                    </div>
+
+                    <div class=" col d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-primary btn-sm">INVIA</button>
+                    </div>
+                </div>
 
 
-    <div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Parking</th>
-                    <th scope="col">Vote</th>
-                    <th scope="col">Distance to center</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $count = 1; //inizializzare un counter per hotel #
-                //controlla se un filtro per parcheggio è selezionato nel GET, altrimenti mostra tutti
 
-                $parkingFilter = isset($_GET['parkingFilter']) ? $_GET['parkingFilter'] : 'all';
 
-                //iterare attraverso hotel in array
-                foreach ($hotels as $hotel) {
-                    //applicare il filtro
-                    if ($parkingFilter == 'all' || ($parkingFilter == 'yes' && $hotel['parking']) || ($parkingFilter == 'no' && !$hotel['parking'])){
-                        
+
+            </form>
+
+        </div>
+
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Distance to center</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $count = 1; //inizializzare un counter per hotel #
                     
-                        echo '<tr>';
-                        //hotel number
-                        echo '<th scope="row">' . $count++ . '</th>';
-                        //nome hotel  
-                        echo '<td>' . $hotel['name'] . '</td>';
-                        //descrizione htl
-                        echo '<td>' . $hotel['description'] . '</td>';
-                        //parcheggio htl con operatore ternario, stampa si se true o no se false
-                        echo '<td> Parking:' . ($hotel['parking'] ? 'yes' : 'no') . '</td>';
-                        //stampa voto hotel
-                        echo '<td> vote:' . $hotel['vote']  . '</td>';
-                        //stampa distanza dal centro
-                        echo '<td> Distance to center:' . $hotel['distance_to_center']  . 'km </td>';
-                        //lchiudere tabella
-                        echo '</tr>';
-                    }
-                }
+                    
+                    //controllare se filtro parcheggio è selezionato e se non lo è, mostra tutti
+                    $parkingFilter =
+                    isset($_GET['parkingFilter']) && $_GET['parkingFilter'] !== '' ? $_GET['parkingFilter'] : 'all';
 
-                ?>
-            </tbody>
-        </table>
+                    //controllare se filtro stella è selezionato , mostra tutti se no
+                    $starsFilter =
+                    isset($_GET['starsFilter']) && $_GET['starsFilter'] !== '' ? $_GET['starsFilter'] : 'all';
+
+                    //iterare attraverso hotel in array
+                    foreach ($hotels as $hotel) {
+                        //applicare il filtro
+                        if (
+                            ($parkingFilter == 'all' || ($parkingFilter == 'yes' && $hotel['parking']) || ($parkingFilter == 'no' && !$hotel['parking'])) &&
+                            ($starsFilter == 'all' || ($starsFilter == '1' && $hotel['vote'] == 1) || ($starsFilter == '2' && $hotel['vote'] >= 2) || ($starsFilter == '3' && $hotel['vote'] >= 3) || ($starsFilter == '4' && $hotel['vote'] >= 4) || ($starsFilter == '5' && $hotel['vote'] == 5))
+                        ) {
+
+
+                            echo '<tr>';
+                            //hotel number
+                            echo '<th scope="row">' . $count++ . '</th>';
+                            //nome hotel  
+                            echo '<td>' . $hotel['name'] . '</td>';
+                            //descrizione htl
+                            echo '<td>' . $hotel['description'] . '</td>';
+                            //parcheggio htl con operatore ternario, stampa si se true o no se false
+                            echo '<td> Parking:' . ($hotel['parking'] ? 'yes' : 'no') . '</td>';
+                            //stampa voto hotel
+                            echo '<td> vote:' . $hotel['vote']  . '</td>';
+                            //stampa distanza dal centro
+                            echo '<td> ' . $hotel['distance_to_center']  . 'km </td>';
+                            //lchiudere tabella
+                            echo '</tr>';
+                        }
+                    }
+
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
 </body>
